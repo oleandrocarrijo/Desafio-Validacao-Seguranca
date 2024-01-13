3c4ffe7b187e61b7bc31c3d0ca0c9529e7675b27
Leandro Carrijo <7leandrobatista@gmail.com>
2024-01-13 14:10:49 -0300

    feat: created Repository, Service and Controller

---
+++ src/main/java/com/devsuperior/demo/controller/CityController.java
@@ -0,0 +1,9 @@
+package com.devsuperior.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/city")
+public class CityController {
+}
